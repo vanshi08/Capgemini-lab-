58170771a17b3b4672e6c8e4cc8fdaea89cdb1ba
vanshi08 <98950197+vanshi08@users.noreply.github.com>
2022-02-04 10:44:31 +0530

    Add files via upload

---
+++ SwitchExample.java
@@ -0,0 +1,36 @@
+package co.vanshika;
+
+public class SwitchExample {
+public static void main(String args[]) {
+        
+        int x = 6;
+        //float y = 3.2f;   // Java considers any decimal number as a double value by default
+        
+        switch(x) {
+        
+        
+       
+            
+        case 1: 
+            System.out.println("1");
+            break;
+        case 2:
+            System.out.println("2");
+            break;
+            
+        case 3:
+            System.out.println("5");
+            break;
+        default:
+            System.out.println("default");
+           break;
+            
+        }
+        
+        
+        
+        
+    } 
+    
+}
+
---
+++ Test.java
@@ -0,0 +1,15 @@
+package co.vanshika;
+
+public class Test
+	{
+		public static void  main(String args[])
+		{
+			int x;
+			for (x = 0; x < 4; x++)
+			{
+				System.out.println(x);
+			}
+
+		System.out.println(x);
+		}
+	}
